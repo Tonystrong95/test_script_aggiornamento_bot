b4bd8715405103e6c0046fb73a8930f60655d97e
Antonio Di Marzo <antoniodimarzo95@libero.it>
2025-11-25 18:16:52 +0100

    quarto test

--- BOT/bot1/bot1.py
+++ BOT/bot1/bot1.py
@@ -1,6 +1,6 @@
 import time
 
-print("Bot in esecuzione... (CTRL+C per terminare)")
+print("Bot1 in esecuzione... (CTRL+C per terminare)")
 
 while True:
     time.sleep(1)
--- BOT/bot2/bot2.py
+++ BOT/bot2/bot2.py
@@ -1,6 +1,6 @@
 import time
 
-print("Bot in esecuzione... (CTRL+C per terminare)")
+print("Bot2 in esecuzione... (CTRL+C per terminare)")
 
 while True:
     time.sleep(1)
